888dfb8d938d69e798daece2e0e90900d0f9f645
Yonerfy Hennep <yonerfy15@gmail.com>
2021-04-15 21:45:30 +0200

    FilterMovies and handleOnChangeEvent

--- app.js
+++ app.js
@@ -5,25 +5,67 @@ const createLiAppendToUl = function (movies) {
     li.append(movies);
 }
 
-const addEventListeners = function () {
-    const radios = document.getElementsByName("film-filter");
-    console.log(radios);
-    radios.forEach(radios => {
-        radios.addEventListener("change", function () {
-            console.log(event.target.value);
-        })
-    })
+const filterMovies = function (movieTitle) {
+    return movies.filter(movie => movie.Title.includes(movieTitle))
 }
 
-const addMoviesToDom = function (movies) {
-
-    for (let i = 0; i < movies.length; i++) {
-        const img = document.createElement("img");
-        img.src = movies[i].Poster;
-        createLiAppendToUl(img);
+const handleOnChangeEvent = function (event) {
+    console.log(event.target.value);
+    if (event.target.value == "Avenger Films") {
+        return filterMovies("Avengers");
+        console.log(filterMovies("Avengers"));
+    }
+    if (event.target.value == "X-Men Films") {
+        return filterMovies("X-Men");
+        console.log(filterMovies("X-Men"));
+    }
+    if (event.target.value == "Princess Films") {
+        return filterMovies("Princess");
+        console.log(filterMovies("Princess"));
+    }
+    if (event.target.value == "Batman Films") {
+        return filterMovies("Batman")
+        console.log(filterMovies("Batman"));
     }
 }
-addMoviesToDom(movies);
+
+const addEventListeners = function () {
+    const radios = document.getElementsByName("film-filter");
+    // console.log(radios);
+    radios.forEach(radios => {
+        radios.addEventListener("change", function () {
+            // console.log(event.target.value);
+            const ul = document.getElementById("movies-places");
+            const filterMovies = handleOnChangeEvent(event);
+            if (ul.hasChildNodes()) {
+                ul.innerHTML = "";
+            }
+            addMoviesToDom(filterMovies);
+        });
+    });
+}
+
+// const addMoviesToDom = function (movies) {
+
+//     for (let i = 0; i < movies.length; i++) {
+//         const img = document.createElement("img");
+//         img.src = movies[i].Poster;
+//         createLiAppendToUl(img);
+//     }
+// }
+const addMoviesToDom = function (movies) {
+
+    const filterMovies = movies.map(movie => {
+        const img = document.createElement("img");
+        img.src = movie.Poster;
+        createLiAppendToUl(img);
+    });
+    return filterMovies;
+}
+
+// addMoviesToDom(movies);
 
 addEventListeners();
-// console.log(addMoviesToDom(movies));
+
+console.log(movies);
+
